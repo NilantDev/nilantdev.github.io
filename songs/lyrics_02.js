15381bf841afabdd5095a081ef1a6985e7ae7cb8
Ilya Antonov <nilantdev@gmail.com>
2024-01-07 22:51:59 +0400

    Change songs

---
+++ songs/lyrics_02.js
@@ -0,0 +1,240 @@
+function getWeekLyrics() {
+    return [
+    {
+        dayOfWeek: 1,
+        lyrics: `
+Моря синяя равнина, волны катят не спеша
+Я скажу тебе, Марина: "До чего ж ты хороша"
+Жёлтый пляж уходит в море, горизонта рвётся нить
+Ты меня полюбишь вскоре, а иначе мне не жить
+Я люблю тебя, Марина, всё сильней день ото дня
+Без твоей любви, Марина, этот мир не для меня
+Наши встречи так недолги, расставания впереди
+Разлетимся, как осколки, ты попробуй-ка найди
+Ты сказала мне, Марина, гордость девичью храня
+Я уйду, как субмарина, если тронешь ты меня
+Что ты милая, не трону, кто я есть в твоей судьбе?
+Лишь прическу, как корону, дай поправлю на тебе
+Призадумалась Марина, вот и кончилась гроза
+И как два аквамарина, смотрят вдаль её глаза
+Разлетимся как осколки ты попробуй-ка найди
+    `
+    },
+    {
+        dayOfWeek: 2,
+        lyrics: `
+Cut my life into pieces
+This is my last resort
+Suffocation, no breathing
+Don't give a fuck if I cut my arm bleeding
+I've reached my last resort
+Do you even care if I die bleeding?
+Would it be wrong, would it be right
+If I took my life tonight?
+Chances are that I might
+Mutilation out of sight
+And I'm contemplating suicide
+'Cause I'm losing my sight, losing my mind
+Wish somebody would tell me I'm fine
+Losing my sight, losing my mind
+I never realized I was spread too thin
+'Til it was too late, and I was empty within
+Hungry, feeding on chaos and living in sin
+Downward spiral, where do I begin?
+It all started when I lost my mother
+No love for myself and no love for another
+Searching to find a love upon a higher level
+Finding nothing but questions and devils
+Nothing's alright, nothing is fine
+I'm running and I'm crying
+I'm crying, I'm crying
+I can't go on living this way
+Can't go on
+Living this way
+Nothing's alright
+`
+    },
+    {
+        dayOfWeek: 3,
+        lyrics: `
+Вот море молодых колышат супербасы,
+Мне триста лет, я выполз из тьмы.
+Они торчат под рейв и чем-то пудрят носы -
+Они не такие, как мы.
+И я не горю желаньем лезть в чужой монастырь,
+Я видел эту жизнь без прикрас.
+Не стоит прогибаться под изменчивый мир,
+Пусть лучше он прогнется под нас.
+Однажды он прогнется под нас.
+Один мой друг - он стоил двух. Он ждать не привык.
+Был каждый день последним из дней.
+Он пробовал на прочность этот мир каждый миг
+Мир оказался прочней.
+Ну что же, спи спокойно, позабытый кумир,
+Ты брал свои вершины не раз.
+Другой держался русла и теченье ловил
+Подальше от крутых берегов.
+Он был, как все и плыл, как все, и вот он приплыл -
+Ни дома, ни друзей, ни врагов.
+И жизнь его похожа на фруктовый кефир,
+Видал я и такое не раз.
+Пусть старая джинса давно затёрта до дыр,
+Пускай хрипит раздолбанный бас.
+       `
+    },
+    {
+        dayOfWeek: 4,
+        lyrics: `
+This ain't a song for the broken-hearted
+No silent prayer for the faith-departed
+I ain't gonna be just a face in the crowd
+You're gonna hear my voice when I shout it out loud
+It's my life, it's now or never
+I ain't gonna live forever
+I just want to live while I'm alive
+It's my life
+My heart is like the open highway
+Like Frankie said I did it my way
+I just wanna live while I'm alive
+This is for the ones who stood their ground
+It's for Tommy and Gina who never backed down
+Tomorrow's getting harder, make no mistake
+Luck, it ain't enough, you've got to make your own breaks
+Better stand tall when they're calling you out
+Don't bend, don't break, baby, don't back down
+It's now or never
+        `
+    },
+    {
+        dayOfWeek: 5,
+        lyrics: `
+Я — маленькая лошадка и мне живётся несладко
+Мне трудно нести мою ношу — настанет день и я её брошу
+Я — маленькая лошадка, но стою очень много денег
+Я везу свою большую повозку с того на этот берег
+Мне хочется плакать, мне хочется смеяться
+Мне хочется прыгать, валяться и брыкаться
+Чтобы были друзья или хотя бы один
+Но я работаю как вол, в моей тележке кокаин
+Я умру очень рано, и я знаю об этом
+Может быть, не весной, может быть, ранним летом
+Я люблю слушать песни и костра нюхать дым
+Но нельзя мне отвлекаться — я везу кокаин
+Я устала ужасно, я хочу отдохнуть
+Съесть мешков десять сена и надолго уснуть
+Я хочу к перелётным птицам вклиниться в
+Клин, но работа важнее — за спиной кокаин
+Мне противно и трудно, но нельзя отступить
+Хоть и хочется есть, хоть и хочется пить
+Мы когда-нибудь за это в адском пламени сгорим
+Но всё это потом, а в данный момент — кокаин
+Мне обидно, и капают слёзы, когда мне под ноги кидают розы
+Когда люди на улицах и в окнах квартир
+Меня встречают и устраивают пир на весь мир
+Мне рады даже малые дети, мне машут даже деревьев ветви
+Меня приветствуют все, все как один — я привезла им новый мир!
+Я привезла кокаин!
+`
+    },
+    {
+        dayOfWeek: 6,
+        lyrics: `
+I work all night, I work all day
+To pay the bills, I have to pay
+Ain't it sad?
+And still there never seems to be
+A single penny left for me
+That's too bad
+In my dreams, I have a plan
+If I got me a wealthy man
+I wouldn't have to work at all
+I'd fool around and have a ball
+Money, money, money
+Must be funny
+In a rich man's world
+Always sunny
+Oh-oh-oh-oh
+All the things I could do
+If I had a little money
+It's a rich man's world
+A man like that is hard to find
+But I can't get him off my mind
+And if he happens to be free
+I bet he wouldn't fancy me
+So I must leave, I'll have to go
+To Las Vegas or Monaco
+And win a fortune in a game
+My life will never be the same
+Oh-oh-oh-oh-oh-oh
+`
+    },
+    {
+        dayOfWeek: 7,
+        lyrics: `
+Ladies and gentlemen
+Добро пожаловать к нам в клуб
+Yo, Dlee, давай жару, поехали (Are you ready?)
+Когда мы в клубе, чиксы танцуют
+Пусть город знает, с кем он тусует
+Встречайте прямо из Москвы
+(А Тимати и DJ Dlee)
+Где лучшие тусовки? — У нас в клубе
+Где лучшие подруги? — У нас в клубе
+Где люди живут хип-хопом? — У нас в клубе
+Если я нужен тебе, ищи меня в клубе (Ещё раз!)
+Если я нужен тебе, ищи меня в клубе
+Килограммы денег просто ни к чему
+Мы покупаем доли в клубах — вот как я живу
+Спроси мою братву, из жёлтых бутылок реки
+Допивая свой rosé, подписываю чеки
+Чики просто в шоке от моей манеры
+Никогда не туплю — всегда подхожу первый
+Как тебя зовут, ма? Ты одна тут, ма?
+Пойдём к нам за стол, нас все уже ждут, ма
+О-оу, чья-то чика за моим столом
+Она пьёт со мной на брудершафт, знаешь, что потом, а?
+Мы танцуем вдвоём в центре этой party
+Окей, малыш, остановись, всё, хватит
+Эй, baby, подскажи, как тебя любить, а?
+Моя конфетка, не смогу тебя забыть (у)
+Люблю твой style, особенно вид сзади
+Я приглашаю тебя на afterparty
+Вижу, ты согласна, это было ясно
+Хоть мама говорила, что быть с Тимати опасно
+Это напрасно, не слушай ерунду
+Я сворую тебя ночью и привезу к утру
+Если любишь жару, если любишь огни
+Глазом подмигни, за собой мани
+И растворившись в V.I.P
+Мы не покинем клуб, пока играет DJ Dlee (Dlee)
+Клубы любят (Тимати)
+Чиксы любят (Тимати)
+Мажоры почему-то тоже уважают (Тимати)
+Зоны любят (Тимати)
+Погоны любят (Тимати)
+А кто меня не любит, вы просто мне завидуете
+Mr. Black Star — золотой ребёнок
+Привык жить в люксе уже с пелёнок
+Звезда киноплёнок, король любой party
+Вы её хотели, ну, нет вопросов, на те
+Ladies, shake, shake, shake, shake your body
+Now, fellows, light it up like it's your last party
+Throw your hands up everybody
+Поднимите руки в воздух, если эта тема катит
+На танцполе тесно, в клубе нету места
+Очередь на входе — у нас опять fiesta
+Фейсконтроль в курсе — у нас опять fiesta
+Ты знаешь style — лохам здесь не место
+У нас в клубе
+Fiesta
+Ha afterparty
+Москва over here
+Лохам здесь не место
+Я из Москвы, brother
+У нас Тимати
+Who the best? DJ Dlee
+Всё
+`
+    },
+]
+}
---
+++ songs/songs_02.js
@@ -0,0 +1,61 @@
+function getWeekSongs() {
+    return [
+    {
+        dayOfWeek: 1,
+        artist: "Филипп Киркоров",
+        song: "Марина",
+        cover: "https://t2.genius.com/unsafe/425x425/https%3A%2F%2Fimages.genius.com%2Fcca24ac1bb96268686cb1fde091cf8da.1000x1000x1.jpg",
+        youtube: "https://www.youtube.com/watch?v=umN4e8J9AO8&ab_channel=%D0%A4%D0%B8%D0%BB%D0%B8%D0%BF%D0%BF%D0%9A%D0%B8%D1%80%D0%BA%D0%BE%D1%80%D0%BE%D0%B2"
+    },
+    {
+        dayOfWeek: 2,
+        artist: "Papa Roach",
+        song: "Last Resort",
+        cover: "https://t2.genius.com/unsafe/425x425/https%3A%2F%2Fimages.genius.com%2Ff862a675f1171799c7baeed78b9c0aad.500x436x1.jpg",
+        youtube: "https://www.youtube.com/watch?v=j0lSpNtjPM8&ab_channel=PapaRoachVEVO"
+    },
+    {
+        dayOfWeek: 3,
+        artist: "Машина Времени",
+        song: "Однажды мир прогнётся под нас",
+        cover: "https://t2.genius.com/unsafe/425x425/https%3A%2F%2Fimages.genius.com%2F54954dd2ff39804607b2a87c3f1a1717.1000x1000x1.png",
+        youtube: "https://www.youtube.com/watch?v=THwPZNo4dy0&ab_channel=%D0%9C%D0%B0%D1%88%D0%B8%D0%BD%D0%B0%D0%92%D1%80%D0%B5%D0%BC%D0%B5%D0%BD%D0%B8"
+    },
+    {
+        dayOfWeek: 4,
+        artist: "Bon Jovi",
+        song: "It's My Life",
+        cover: "https://t2.genius.com/unsafe/425x425/https%3A%2F%2Fimages.genius.com%2Fa1daf0a66c39ad5b499ae9252d6c8413.500x500x1.jpg",
+        youtube: "https://www.youtube.com/watch?v=vx2u5uUu3DE&ab_channel=BonJoviVEVO"
+    },
+    {
+        dayOfWeek: 5,
+        artist: "Найк Борзов",
+        song: "Лошадка",
+        cover: "https://t2.genius.com/unsafe/425x425/https%3A%2F%2Fimages.genius.com%2F2087a2ca785077ae443b1d454e399c8f.500x500x1.jpg",
+        youtube: "https://www.youtube.com/watch?v=4qcPopWKJlQ&ab_channel=%D0%9D%D0%B0%D0%B9%D0%BA%D0%91%D0%BE%D1%80%D0%B7%D0%BE%D0%B2"
+    },
+    {
+        dayOfWeek: 6,
+        artist: "ABBA",
+        song: "Money, Money, Money",
+        cover: "https://t2.genius.com/unsafe/425x425/https%3A%2F%2Fimages.genius.com%2F4f5363e88e6456b07194538b98584404.1000x1000x1.png",
+        youtube: "https://www.youtube.com/watch?v=ETxmCCsMoD0&ab_channel=AbbaVEVO"
+    },
+    {
+        dayOfWeek: 7,
+        artist: "Тимати",
+        song: "В клубе",
+        cover: "https://t2.genius.com/unsafe/425x425/https%3A%2F%2Fimages.genius.com%2Fc6bac335c84db8ce26caa7b9e67c3c51.1000x1000x1.png",
+        youtube: "https://www.youtube.com/watch?v=jYqsEj9MKS4&ab_channel=StarPro"
+    },
+]
+}
+
+
+
+
+
+
+
+

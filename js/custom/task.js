3c1d3d10db32014af9f7d0d3d6407308694b220f
Ilya Antonov <nilantdev@gmail.com>
2024-01-15 00:13:59 +0400

    Remove hardcode

--- js/custom/task.js
+++ js/custom/task.js
@@ -1,7 +1,7 @@
 var Task = {
     getTodayTask: function(songs, lyrics) {
         const date = new Date();
-        const dayOfWeek = 7;//date.getDay() == 0 ? 7 : date.getDay();
+        const dayOfWeek = date.getDay() == 0 ? 7 : date.getDay();
         const lyricsArray = lyrics[dayOfWeek - 1]['lyrics'].split('\n');
 
         let result = songs[dayOfWeek - 1];

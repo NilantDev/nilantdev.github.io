343d00389798638ecf845a847d9d978f83b9e7b9
Ilya Antonov <nilantdev@gmail.com>
2024-12-24 22:36:33 +0400

    Add message for updating inforamtion

--- babelib/js/custom/update.js
+++ babelib/js/custom/update.js
@@ -1,20 +1,26 @@
 async function update() {
     const lastVersion = 1;
     let version = getValueFromLocalStorage('version');
-    
+    let messageElement = document.getElementById('update-message');
     if (!version) version = 0;
 
     if (version < 1) {
-        await updateV1();
+        messageElement.innerHTML = `Обновление ${version} / ${lastVersion}`;
+        await updateV1(version, lastVersion);
+        version++;
+        saveInLocalStorage('version', lastVersion);
+        messageElement.innerHTML = `Обновление ${version} / ${lastVersion}`;
     }
 
-    saveInLocalStorage('version', lastVersion);
+    messageElement.innerHTML = ``;
 }
 
-async function updateV1() {
+async function updateV1(version, lastVersion) {
     for (i = 35; i < 116; i++) {
+        let messageElement = document.getElementById('update-message');
+        messageElement.innerHTML = `Обновление ${version} / ${lastVersion} - ${i} / 116`;
         let item = getLocalStorageItem(i);
-        if (Object.keys(item).length !== 0) {
+        if (Object.keys(item).length !== 0 && !Object.values(item).includes('won')) {
             let task = await getTask(i);
             let listItem = await getListItem(task.fantlab_id);
             let title = listItem.author + ' - ' + listItem.title;
